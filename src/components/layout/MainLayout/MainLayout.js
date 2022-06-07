8033b70235a4e3dcaf675698fe00bcf383e0d0d8
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2022-06-07 20:35:13 +0200

    Create MainLayout and modify MainPage

---
+++ src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Header from '../Header/Header';
+
+const MainLayout = ({ children }) => (
+  <>
+    <Header />
+    {children}
+  </>
+);
+
+MainLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+export default MainLayout;
--- src/components/views/MainPage/MainPage.js
+++ src/components/views/MainPage/MainPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from '../../layout/Header/Header';
+import MainLayout from '../../layout/MainLayout/MainLayout';
 
 const MainPage = () => (
-  <div>
-    <Header />
-  </div>
+  <MainLayout>
+
+  </MainLayout>
 );
 
 export default MainPage;
