c3572b2e48edf1b8eaf82bb8b2d734844b6acc41
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2022-06-19 22:54:23 +0200

    Modify button common component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,14 +1,11 @@
-import clsx from 'clsx';
 import React from 'react';
 import styles from './Button.module.scss';
 
 const Button : React.FC<ButtonProps> = ({ content, children, OnClickFunc }) => (
   <div className={styles.button} onClick={OnClickFunc}>
-    {children}
-    <p className={children !== undefined
-      ? clsx(styles.inactive) : clsx(styles.content)}>
-      {content}
-    </p>
+    <div className={styles.content}>
+      {children !== undefined ? children : content}
+    </div>
   </div>
 );
 
